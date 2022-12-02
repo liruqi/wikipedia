5934496b4cdfc3568a3388f2a290630fef255fd0
Ruqi <liruqi@gmail.com>
2022-12-02 19:28:44 +0700

    Put sortable to class attrib

--- wikitable.py
+++ wikitable.py
@@ -21,29 +21,49 @@ LANG=sys.argv[1]
 site = pywikibot.Site(LANG, 'wikipedia') 
 BASEDIR="../../wikipedia." + LANG
 ERRORDIR=os.path.join(BASEDIR, "Errors")
-WT_PATTERN=r'{\|(\s*)table'
 names = [f for f in os.listdir(ERRORDIR)]
 cnt=0
+action='beginning'
+debug=False
+if len(sys.argv)>2:
+    action=sys.argv[2]
+
+WT_BEGIN_PATTERN=r'{\|(\s*)table'
+WT_BEGIN_TEXT='{|'
+
+WT_SOURCE_PATTERN=r'{\|(\s*)class\=wikitable(\s*)sortable(\s*)(\w+)='
+WT_SOURCE_TEXT=r'{| class="wikitable sortable" \4='
+
+S_PATTERN=WT_BEGIN_PATTERN
+R_TEXT=WT_BEGIN_TEXT
+if action[0]=='s':
+    S_PATTERN=WT_SOURCE_PATTERN
+    R_TEXT=WT_SOURCE_TEXT
+    #debug=True
+
 for name in names:
     if name[-8:] == ".err.log":
         cnt+=1
         d=0
         with open (os.path.join(ERRORDIR, name)) as f:
             err=f.read()
-            mo=re.search(WT_PATTERN,err)
+            mo=re.search(S_PATTERN,err)
             if mo == None:
                 continue
             print(mo.group(0))
             print(cnt, "Found:", name[:-8], "->", err)
             page = pywikibot.Page(site, name[:-8])
             ptext=page.text
-            ntext=re.sub(WT_PATTERN, '{|', ptext)
+            ntext=re.sub(S_PATTERN, R_TEXT, ptext)
             if ptext == ntext:
                 print("wikitable error not found.")
                 os.remove(os.path.join(ERRORDIR, name))
                 continue
             page.text=ntext
-            page.save('Fix wikitable beginning syntax, src: https://github.com/liruqi/wikipedia/blob/master/wikitable.py')
+            if debug:
+                print (ntext)
+            else:
+                page.save('Fix wikitable ' + action + ' syntax, src: https://github.com/liruqi/wikipedia/blob/master/wikitable.py')
             wt=os.path.join(ERRORDIR, name[:-8]+".wikitext")
             if os.path.isfile(wt) and LANG=="zh":
                 with open(wt,"w") as wtf:
